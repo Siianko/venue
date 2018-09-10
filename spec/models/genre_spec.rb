f806884f26bb4e8118e9dabb1e68ffccf7db6878
Siianko <tsiyanko@gmail.com>
2018-09-10 13:17:09 +0200

    adding factory spec for genres

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
+    
+    describe 'Associations' do
+        it { is_expected.to belong_to :user }
+    end
+
+    describe 'Factory' do
+        it 'should have valid Factory' do
+          expect(create(:genre)).to be_valid 
+        end
   
 end
